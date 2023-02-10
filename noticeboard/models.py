1025e55ef29025f130adec59f980316561fd555e
Dyerrrr <69588466+Dyerrrr@users.noreply.github.com>
2023-02-10 21:02:36 +0000

    Created 'Notice' model

--- noticeboard/models.py
+++ noticeboard/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Notice(models.Model):
+    datetime_of_creation = models.DateTimeField(verbose_name='Created on', auto_now=True)
+    title = models.CharField(verbose_name='Title', max_length=30)
+    description = models.TextField()
+    active = models.BooleanField(verbose_name='The notice is active')
+    pin = models.BooleanField(verbose_name='The notice is pinned')
+
+    def __str__(self):
+        return self.title
